04a24cb7515cbcacefc18b175ce30d57ec5dc397
jade decordova <lacroixjaded@gmail.com>
2020-08-10 02:12:59 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -8,9 +8,19 @@ class school
   end
   
   def  add_student(student_name, grade)
-   @roster <<  {:student_name => grade}
+   @roster[grade]= []
+   @roster[grade] << student_name
    @roster
  end
+ 
+ def grade(num)
+   @roster[num]
+  end
+  
+  def sort 
+    @roster.sort
+  end
+end
  
  
     
