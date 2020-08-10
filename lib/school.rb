55d39b234c67ea609481926acffc79b47dacd56b
jade decordova <lacroixjaded@gmail.com>
2020-08-10 02:05:41 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,16 @@
-# code here!
+class school
+  attr_accessor :roster
+  attr_reader :name
+  
+  def initialize (name)
+    @name = name
+    @roster = {}
+  end
+  
+  def  add_student(student_name, grade)
+   @roster <<  {:student_name => grade}
+   @roster
+ end
+ 
+ 
+    
